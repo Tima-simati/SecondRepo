abc778dfd1b35d4a2cfbb997920d2c648c611859
Tima-simati <timaaks0ul@gmail.com>
2026-07-22 23:19:41 +0200

    if array is 4x4, then exlusively coloring has been added

--- Arrays_2D/Arrays_2D/Program.cs
+++ Arrays_2D/Arrays_2D/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Pastel;
+using System;
 using System.Drawing;
-using Console = Colorful.Console;
 
 namespace Arrays_2D
 {
@@ -8,7 +8,6 @@ namespace Arrays_2D
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("This is a program for displaying arrays.\nWhat should be the dimensions of your array?");
             Console.WriteLine("Please enter the number of rows.");
             int rows = Convert.ToInt16(Console.ReadLine());
@@ -17,8 +16,12 @@ namespace Arrays_2D
             //array initialized after user input with no values
             string[,] array2D = new string[rows, columns];
             //setting up variables for putting grid in the array
-            int gridColumns = columns * 4 + 1;
+            int gridColumns = columns * 4 + 1; // because of grid upper and lower boundaries +---+
             string gridLines = "+";
+            //RGB variables for color:
+            int r = 255;
+            int g = 255;
+            int b = 255;
             for (int i = 1; i < gridColumns; i++)
             {
                 if (i % 4 == 0)
@@ -34,7 +37,7 @@ namespace Arrays_2D
             Console.WriteLine("Do you prefer the contents of the array should be numbers, alternating symbols or indices?\nPress 1 for numbers. 2 for symbols. 3 for showing indices as content.");
             Console.WriteLine("You chose:");
             char arraysContentSelection = Console.ReadKey().KeyChar;
-            Console.WriteLine("\nYour array is:");
+
             int runningValueIncrement = 0;
             //numbers selected as content for 2D Array
             if (arraysContentSelection == '1')
@@ -77,8 +80,10 @@ namespace Arrays_2D
                     }
                 }
             }
-            Console.WriteLine("Do you want to have a grid for your array or not? Press y or n.");
+            Console.WriteLine("\nDo you want to have a grid for your array or not? Press y or n.");
             string gridSelection = Console.ReadLine().ToLower();
+            Console.WriteLine();
+            Console.WriteLine("\nYour array is:");
             if (gridSelection == "y")               //outputting of 2D array to Console with grid
             {
                 Console.WriteLine(gridLines);
@@ -87,7 +92,30 @@ namespace Arrays_2D
                     for (int j = 0; j < columns; j++)
                     {
                         Console.Write("|");
-                        Console.Write($"{array2D[i, j],3}");
+                        if (rows == 4)
+                        {
+                            if (j % columns == 0)
+                            {
+                                Console.Write($"{array2D[i, j],3}".Pastel(Color.FromArgb(r, 0, 0)));
+                            }
+                            if (j % columns == 1)
+                            {
+                                Console.Write($"{array2D[i, j],3}".Pastel(Color.FromArgb(0, g, 0)));
+                            }
+                            if (j % columns == 2)
+                            {
+                                Console.Write($"{array2D[i, j],3}".Pastel(Color.FromArgb(0, 0, b)));
+                            }
+                            if (j % columns == 3)
+                            {
+                                Console.Write($"{array2D[i, j],3}".Pastel(Color.FromArgb(r, g, 0)));
+                            }
+                        }
+                        else
+                        {
+                            Console.Write($"{array2D[i, j],3}");
+                        }
+
                     }
                     Console.WriteLine("|");
                     Console.WriteLine(gridLines);
@@ -99,7 +127,7 @@ namespace Arrays_2D
                 {
                     for (int j = 0; j < columns; j++)
                     {
-                        Console.Write($"{array2D[i, j]} ");
+                        Console.Write($"{array2D[i, j],3}");
                     }
                     Console.WriteLine($"");
                 }
